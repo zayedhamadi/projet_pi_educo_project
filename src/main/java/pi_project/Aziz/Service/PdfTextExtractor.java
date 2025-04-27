472cc1340c0a2368463a531e4d336b387e571386
saifDaboussi <saif.daboussi5@gmail.com>
2025-04-27 14:00:04 +0100

    fix error

--- src/main/java/pi_project/Aziz/Service/PdfTextExtractor.java
+++ src/main/java/pi_project/Aziz/Service/PdfTextExtractor.java
@@ -1,6 +1,7 @@
 package pi_project.Aziz.Service;
 
-import org.apache.pdfbox.Loader;
+
+
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
 import java.io.File;
@@ -8,7 +9,9 @@ import java.io.IOException;
 
 public class PdfTextExtractor {
     public static String extractText(String filePath) throws IOException {
-        PDDocument document = Loader.loadPDF(new File(filePath));
+        PDDocument document = PDDocument.load(new File(filePath));
+
+
         try {
             return new PDFTextStripper().getText(document);
         } finally {
